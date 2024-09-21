7fad0d5e3dbcc3c98c985fb4a20572226ea66b93
GbsoDev <gb.so.dev@outlook.com>
2024-09-21 15:30:35 -0500

    Funciones: Estados compuestos con código imperativo

--- src/UseState.js
+++ src/UseState.js
@@ -2,54 +2,107 @@ import React from "react";
 
 const SECURITY_CODE = 'paradigma';
 function UseState({ name }) {
-  const [error, setError] = React.useState(false);
-  const [value, setValue] = React.useState('');
-  const [loading, setLoading] = React.useState(false);
+  const [state, setState] = React.useState({
+    error: false,
+    value: '',
+    loading: false,
+    delete: false,
+    confirmed: false,
+  });
 
+  console.log(state);
   React.useEffect(() => {
-    if (!!loading) {
+    if (!!state.loading) {
       setTimeout(() => {
         try {
           console.log("Empezando el efecto");
-          if (value !== SECURITY_CODE) {
+          if (state.value !== SECURITY_CODE) {
             throw new Error("El código es incorrecto")
           } else {
-            setError(false);
+            setState(prevstate => ({
+              ...prevstate,
+              error: false,
+              delete: true,
+            }));
           }
         } catch (error) {
           console.log("Error en el efecto");
-          setError(true)
+          setState(prevstate => ({
+            ...prevstate,
+            error: true,
+          }));
         }
         finally {
           console.log("Finalizando el efecto");
-          setLoading(false);
+          setState(prevstate => ({
+            ...prevstate,
+            loading: false,
+          }));
         }
       }, 2000);
     }
-  }, [loading])
+  }, [state.loading])
 
-  return (
-    <div>
-      <h2>Eliminar {name}</h2>
-      <p>Por favor, escribe el código de seguridad</p>
+  if (!state.delete && !state.confirmed) {
+    return (
+      <div>
+        <h2>Eliminar {name}</h2>
+        <p>Por favor, escribe el código de seguridad</p>
 
-      {!!error && !loading && (
-        <p>Error: El código es incorrecto</p>
-      )}
+        {!!state.error && !state.loading && (
+          <p>Error: El código es incorrecto</p>
+        )}
 
-      {!!loading && (
-        <p>Cargando...</p>
-      )}
+        {!!state.loading && (
+          <p>Cargando...</p>
+        )}
 
-      <input placeholder="Código de seguridad"
-        value={value} onChange={(event) => setValue(event.target.value)}
-      />
+        <input placeholder="Código de seguridad"
+          value={state.value} onChange={
+            (event) => setState({
+              ...state,
+              value: event.target.value
+            })
+          }
+        />
 
-      <button
-        onClick={() => setLoading(true)}
-      >Comprobar</button>
-    </div>
-  );
+        <button
+          onClick={() => setState({
+            ...state,
+            loading: true,
+          })}
+        >Comprobar</button>
+      </div>
+    );
+  } else if (!!state.delete && !state.confirmed) {
+    return (
+      <>
+        <p>Confirma la eliminación</p>
+        <button onClick={() => setState({
+          ...state,
+          confirmed: true,
+        })}
+        >Confirmar</button>
+        <button onClick={() => setState({
+          ...state,
+          delete: false,
+        })}
+        >Cancelar</button>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <p>Eliminado con éxito</p>
+        <button onClick={() => setState({
+          ...state,
+          delete: false,
+          confirmed: false,
+        })}
+        >Volver</button>
+      </>
+    );
+  }
 }
 
 export { UseState };
