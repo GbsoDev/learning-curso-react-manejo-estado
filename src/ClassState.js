98cbb023a443fd57af95413eff751b9f5c20e817
GbsoDev <gb.so.dev@outlook.com>
2024-09-21 13:32:56 -0500

    Funciones y clases: formas de usar un estado simple

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,14 +1,31 @@
 import React from "react";
 
 class ClassState extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: false
+    }
+  }
+
   render() {
     const { name } = this.props;
+    const { error } = this.state;
     return (
       <div>
         <h2>Eliminar {name}</h2>
         <p>Por favor, escribe el código de seguridad</p>
+
+        {!!error && (
+          <p>Error: El código es incorrecto</p>
+        )}
+
         <input placeholder="Código de seguridad" />
-        <button>Comprobar</button>
+        <button
+          onClick={() => 
+            this.setState(prevState => ({ error: !prevState.error }))
+          }
+        >Comprobar</button>
       </div>
     );
   };
