04e37b89b4c09789f0278110ee818fc90cbf3072
GbsoDev <gb.so.dev@outlook.com>
2024-09-21 13:09:53 -0500

    Funciones y Clases: Declarando mismo componente mediante el uso de funciones y clases

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { UseState } from './UseState.js'
+import { ClassState } from './ClassState.js'
 
 function App() {
   return (
     <div className="App">
+      <UseState />
+      <ClassState />
     </div>
   );
 }
